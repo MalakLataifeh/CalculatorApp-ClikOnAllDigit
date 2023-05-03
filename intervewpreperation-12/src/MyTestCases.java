ac8956ae844e8ad1375839ac17b68ccd39de3e8d
MalakLataifeh <130087902+MalakLataifeh@users.noreply.github.com>
2023-05-03 23:58:49 +0400

    Update MyTestCases.java

--- intervewpreperation-12/src/MyTestCases.java
+++ intervewpreperation-12/src/MyTestCases.java
@@ -45,8 +45,7 @@ public class MyTestCases extends parameter {
 	}
 	*/
 	
-	//lecture13
-	////////////////////////////////////////
+	
 	
 	
 	/*@Test()
@@ -67,20 +66,13 @@ public class MyTestCases extends parameter {
 	/*
 	 @Test()
 	 public void logo () {
-		//hai lma a3ml click 3l logo ka svg ma r7 yt3rf 3leha 
+		
 		// driver.findElement(By.xpath("//*[@id=\"__next\"]/footer/div[3]/div[3]/div[1]/div[2]/div/div[1]/svg[2]")).click();
-		 
-		//la hek r7 ast5dm al div eli be7tawi had al logo
-		 
+	
 		 WebElement divContainsVsaLogo = driver.findElement(By.xpath("//*[@id=\"__next\"]/footer/div[3]/div[3]/div[1]/div[2]/div/div[1]"));
-		 
-		 //hl2 r7 a5le ydower joa had el div 3la ai eshi mn no3 svg 
 		 List<WebElement> mydiv= divContainsVsaLogo.findElements(By.tagName("svg"));
 		 System.out.println(mydiv.size());
-		 
-		 //hon 7katlo yjebli l value tab3t el attribute datatestid
 		String ActualVsaID =  mydiv.get(1).getAttribute("data-testid");
-		
 		String ExpectedVsaID = "Footer__VisaCardLogo";
 		 myassertion.assertEquals(ActualVsaID, ExpectedVsaID);
 	     myassertion.assertAll();
@@ -89,7 +81,7 @@ public class MyTestCases extends parameter {
 	
 	/*
 	@Test()
-	 public void  sobhiTest() {
+	 public void  TestTwo() {
 		 
 		WebElement Vsa = driver.findElement(By.cssSelector("[data-testid=\"Footer__VisaCardLogo\"]"));
 		Vsa.isDisplayed();
@@ -98,113 +90,6 @@ public class MyTestCases extends parameter {
 		
 	} */
 	
-	
-	//Ahmadtest
-	
-	
-	
-	
-	///////////////////////////////////////////
-	//Array
-	
-	/*
-	@Test()
-	public void myTest()
-	{
-	//static array
-	int myarrayOne[] = new int [10];
-	for(int i=0; i< myarrayOne.length;i++)
-	{
-		myarrayOne[i]=3;
-		
-	}
-	*/
-	
-	
-	
-	/*
-	@Test()
-	public void myTest()
-	{
-	//dynamic array//hon bi hai el array ma blzmni bi size mo3an
-		
-		
-	//int myarraytwo[]= {};
-	
-	//hai hon  lo tab3tha jowa el for r7 y3tene enha out of index l2no el lenght zero fa ma b9er t7ki index 90 direkt la hek r7 ast5dm el for 
-	//myarraytwo[90]=33;
-	/*for (int i= 0 ;i<myarraytwo.length;i++)
-	{
-	System.out.println(myarraytwo.length);
-		
-	}}*/
-	
-	/*
-	@Test()
-	public void myTest() {
-	//hai hak el dynamic lazem a7ot el 8yam ben el {}	
-int myarraytwo[]= {0,1,2,3,9,11,33,33,44,22,11,39};
-
-	}*/
-	
-	
-	
-	/*
-	
-	//list
-	@Test()
-	public void mytestThree()
-	{
-	//List<Integer> mylistThree = new ArrayList<Integer>();
-	//List<WebElement> mylistFour = driver.findElements(By.tagName("a"));   //a is link 
-	//List<WebElement> mylistFour = driver.findElements(By.tagName("img"));
-    //System.out.println(mylistFour.size());
-		int myarraytwo[]= {0,1,2,3,9,11,33,33,44,22,11,39};
-		List<Integer> mylistFive = new ArrayList<Integer>() ; 
-	
-		for(int i=0;i<myarraytwo.length;i++)
-		{
-		mylistFive.add(myarraytwo[i]);
-		//hai r7 kol marah ytb3li el size 1,2,3,4,..,12
-		System.out.println(mylistFive.size());
-			
-			
-		}
-		
-	}
-		*/
-	
-	
-	
-	
-	
-	
-	
-	
-	/*
-		
-		@Test()
-		public void mytestFour() {
-//		List<Integer> mylistThree = new ArrayList<Integer>();
-//		mylistThree.add(100);
-//		System.out.println(mylistThree.get(0));
-//			
-			
-			//this will get all pic in website and print it
-			
-			List<WebElement> mylistFour = driver.findElements(By.tagName("img"));
-			for(int i=0;i<mylistFour.size();i++) {
-				
-				String pic=  mylistFour.get(i).getAttribute("src");
-				System.out.println(pic);
-			}
-			
-		}
-		*/
-		
-		
-		
-		/*
 		@Test()
 		public void  hotel_tab_is_not_selected()
 		{
@@ -218,7 +103,7 @@ int myarraytwo[]= {0,1,2,3,9,11,33,33,44,22,11,39};
 			String Actualvalue =  hotleTab.getAttribute("aria-selected");
 			myassertion.assertEquals(Actualvalue, Expectedvalue);
 			myassertion.assertAll();
-		}*/
+		}
 	
 /*	
 	@Test(invocationCount = 5)
@@ -254,7 +139,7 @@ int myarraytwo[]= {0,1,2,3,9,11,33,33,44,22,11,39};
 	
 	@Test(invocationCount = 3)
 	public void Randomly_select_number_of_visitor() throws InterruptedException {
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3)); //hai 9'ftha 3shan lma enta8l mn tab el flight la tab el hotel b3den edwor 3l select bdonha r7 y9'rob error l2no ma l78
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3)); 
 		Random myRandom = new Random();
 		int myRadoooom = myRandom.nextInt(2);
 		driver.findElement(By.xpath("//*[@id=\"uncontrolled-tab-example-tab-hotels\"]")).click(); //hotle tab
@@ -273,9 +158,9 @@ int myarraytwo[]= {0,1,2,3,9,11,33,33,44,22,11,39};
 		
 	
 	
-//		@AfterTest
-//		public void closeTheBrowser()
-//		{
-//			driver.close();
-//		}
+		@AfterTest
+		public void closeTheBrowser()
+		{
+			driver.close();
+		}
 }
